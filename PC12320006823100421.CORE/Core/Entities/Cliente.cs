ecbe14b56270fd2e2c765521d1c6e127453de4d5
Erick Cochachi Ramos <erick.cochachi.ramos@gmail.com>
2026-05-28 19:07:26 -0500

    Añade entidades y DbContext, reorganiza proyectos y refs
    Se agregan las entidades principales y el DbContext para EF Core.
    Se reorganizan carpetas en CORE y se actualizan referencias
    a paquetes y proyectos para reflejar la nueva estructura.

---
+++ PC12320006823100421.CORE/Core/Entities/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PC12320006823100421.CORE.CORE.Entities;
+
+public partial class Cliente
+{
+    public int Id { get; set; }
+
+    public string Paterno { get; set; } = null!;
+
+    public string Materno { get; set; } = null!;
+
+    public string Nombres { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string Telefono { get; set; } = null!;
+
+    public virtual ICollection<Vehiculo> Vehiculo { get; set; } = new List<Vehiculo>();
+}
---
+++ PC12320006823100421.CORE/Core/Entities/OrdenServicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PC12320006823100421.CORE.CORE.Entities;
+
+public partial class OrdenServicio
+{
+    public int Id { get; set; }
+
+    public DateOnly FechaIngreso { get; set; }
+
+    public string DescripcionProblema { get; set; } = null!;
+
+    public decimal CostoEstimado { get; set; }
+
+    public string Estado { get; set; } = null!;
+
+    public int VehiculoId { get; set; }
+
+    public int TipoServicioId { get; set; }
+
+    public virtual TipoServicio TipoServicio { get; set; } = null!;
+
+    public virtual Vehiculo Vehiculo { get; set; } = null!;
+}
